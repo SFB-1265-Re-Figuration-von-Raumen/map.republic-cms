59f11d9b5b236fb2a3a57438e4b7ae956923cb7a
gilb3rt0 <hello@gilb3r.to>
2023-08-15 08:33:48 +0200

    icon category as relation + dependencies upgrade

---
+++ src/api/icon-category/services/icon-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * icon-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::icon-category.icon-category');

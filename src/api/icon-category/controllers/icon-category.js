c19e27bf149f4f4651a18df28337032ef2775fa0
1wrnr <130104955+1wrnr@users.noreply.github.com>
2023-08-15 09:31:18 +0200

    Revert "icon category as relation + dependencies upgrade"

--- src/api/icon-category/controllers/icon-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * icon-category controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::icon-category.icon-category');
--- src/api/icon-category/services/icon-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * icon-category service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::icon-category.icon-category');

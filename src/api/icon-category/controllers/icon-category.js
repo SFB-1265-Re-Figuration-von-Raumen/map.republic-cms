979373a7f47e3378251ec14a87451ebf0be121b1
1wrnr <info@wrnr.dev>
2023-08-17 15:26:13 +0200

    add item category collection type, relation field in icon collection type

---
+++ src/api/icon-category/controllers/icon-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * icon-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::icon-category.icon-category');
